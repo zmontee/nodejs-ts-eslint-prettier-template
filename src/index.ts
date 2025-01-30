6b5f93d312538fd1619bc1407755b34691b93174
zmontee <zmontee@gmail.com>
2025-01-30 19:01:41 +0200

    tsx + dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as process from 'node:process';
+
+dotenv.config();
+
+const app = express();
+
+console.log('ex');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 2998);
